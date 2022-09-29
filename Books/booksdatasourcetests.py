7918d8e36a524f817690c05273202129f0f16155
Jack Owens <jackowens@MacBook-Pro-22.local>
2022-09-29 18:53:57 -0500

    Debugged tests

--- Books/booksdatasourcetests.py
+++ Books/booksdatasourcetests.py
@@ -23,48 +23,43 @@ class BooksDataSourceTester(unittest.TestCase):
         books = self.data_source.books('Moby Dick')
         self.assertTrue(len(books) == 1)
         self.assertTrue(books[0] == Book('Moby Dick'))
-    
-    def test_equal_booksByYear_emptyStringBooksBetweenYears(self):
-        books_between_years = self.data_source.books_between_years(None, None)
-        books = self.data_source.books(None, 'year')
-        self.assertTrue(books_between_years == books)
 
     def test_startYear_lessThan_endYear(self):
-        self.assertRaises(ValueError, self.books_between_years(2, 1))
+        self.assertRaises(ValueError, lambda: self.data_source.books_between_years('2', '1'))
 
     def test_nonIntegerInput_years(self):
-        self.assertRaises(TypeError, self.data_source.books_between_years('abc', 2))
-        self.assertRaises(TypeError, self.data_source.books_between_years(1, 'abc'))
-        self.assertRaises(TypeError, self.data_source.books_between_years(1.8, 2))
+        self.assertRaises(TypeError, lambda: self.data_source.books_between_years('abc', 2))
+        self.assertRaises(TypeError, lambda: self.data_source.books_between_years(1, 'abc'))
+        self.assertRaises(TypeError, lambda: self.data_source.books_between_years(1.8, 2))
 
     def test_nonStringInput_authors(self):
-        self.assertRaises(TypeError, self.data_source.authors(2))
-        self.assertRaises(TypeError, self.data_source.authors(2.4))
-        self.assertRaises(TypeError, self.data_source.authors(True))
+        self.assertRaises(TypeError, lambda: self.data_source.authors(2))
+        self.assertRaises(TypeError, lambda: self.data_source.authors(2.4))
+        self.assertRaises(TypeError, lambda: self.data_source.authors(True))
 
     def test_nonStringInput_books(self):
-        self.assertRaises(TypeError, self.data_source.books(2))
-        self.assertRaises(TypeError, self.data_source.books(2.4))
-        self.assertRaises(TypeError, self.data_source.books(True))
+        self.assertRaises(TypeError, lambda: self.data_source.books(2))
+        self.assertRaises(TypeError, lambda: self.data_source.books(2.4))
+        self.assertRaises(TypeError, lambda: self.data_source.books(True))
 
     def test_AuthorEqual(self):
-        author = self.data_source.Author('Pratchett', 'Terry')
-        self.AssertTrue(self.data_source.Author.__eq__(author, author))
+        author = self.data_source.authors('Pratchett')[0]
+        self.assertTrue(Author.__eq__(author, author))
 
     def test_BookEqual(self):
-        book = self.data_source.Book('Moby Dick')
-        self.AssertTrue(self.data_source.Book.__eq__(book, book))
+        book = self.data_source.books('Moby Dick')[0]
+        self.assertTrue(Book.__eq__(book, book))
         
     def test_startYear_minimum(self):
-        minimum1900 = self.data_source.books_between_years(1900, None)
-        self.assertEqual(0, minimum1900.count(self.data_source.Book('Emma')))
+        minimum1900 = self.data_source.books_between_years('1900')
+        self.assertEqual(0, minimum1900.count(Book('Emma')))
 
     def test_endYear_maximum(self):
-        maximum2000 = self.data_source.books_between_years(None, 2000)
-        self.assertEqual(0, maximum2000.count(self.data_source.Book('All Clear')))
+        maximum2000 = self.data_source.books_between_years(None, '2000')
+        self.assertEqual(0, maximum2000.count(Book('All Clear')))
 
     def test_default_books(self):
-        self.assertEqual(self.data_source.books(None), self.data_source.books(None, 'title'))
+        self.assertEqual(self.data_source.books('', ''), self.data_source.books('', 'title'))
 
 if __name__ == '__main__':
     unittest.main()
