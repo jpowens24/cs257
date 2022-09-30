c44fbd2c88a3bd7ceb6a54a80253026190013546
Jack Owens <jackowens@MacBook-Pro-22.local>
2022-09-30 08:53:38 -0500

    Debugged booksinrange and wrote exceptions for no arguments or an invalid argument

--- Books/books.py
+++ Books/books.py
@@ -8,9 +8,18 @@ from booksdatasource import BooksDataSource
 
 import sys
 
-if sys.argv[1] == 'books':
+def printUsageStatement():
+    print('NAME\n\tbooks - Search for books \nSYNOPSIS\n\tbooks <S> <C>\nDESCRIPTION\n\tbooks takes a search string <S> and prints a list of books whose titles contain the string and sorts by criteria <C>.\n\t <C> can be <title> or <year>\n')
+    print('NAME\n\tauthors - Search for authors\nSYNOPSIS\n\tauthors <S>\nDESCRIPTION\n\tauthors takes a search string S and prints a list of authors whose names contain the string sorted by last name then first name\n')
+    print('NAME\n\tbooksinrange - Search range of years\nSYNOPSIS\n\tbooksinrange <A> <B>\nDESCRIPTION\n\tbooksinrange prints a list of books published between years <A> and <B>, inclusive\n')
+    print('NAME\n\tuse - Request usage statement\nSYNOPSIS\n\tuse <flag>\nDESCRIPTION\n\tGives user description of <flag> command-line argument')
+
+if len(sys.argv) == 1:
+    print('\nInvalid Input: no arguments\n')
+    printUsageStatement()
+elif sys.argv[1] == 'books':
     if len(sys.argv) > 4:
-        print('Syntax Error: too many arguments for books')
+        print('\nSyntax Error: too many arguments for books\n')
         print('NAME\n\tbooks - Search for books \nSYNOPSIS\n\tbooks <S> <C>\nDESCRIPTION\n\tbooks takes a search string <S> and prints a list of books whose titles contain the string and sorts by criteria <C>.\n\t <C> can be <title> or <year>')
     else:
         if len(sys.argv) == 2:
@@ -23,7 +32,7 @@ if sys.argv[1] == 'books':
             print(book.title + ' ' + book.publication_year)
 elif sys.argv[1] == 'authors':
     if len(sys.argv) > 4:
-        print('Syntax Error: too many arguments for authors')
+        print('\nSyntax Error: too many arguments for authors\n')
         print('NAME\n\tauthors - Search for authors\nSYNOPSIS\n\tauthors <S>\nDESCRIPTION\n\tauthors takes a search string S and prints a list of authors whose names contain the string sorted by last name then first name')
     else:
         if len(sys.argv) == 2:
@@ -37,32 +46,29 @@ elif sys.argv[1] == 'authors':
                 print(author.given_name + ' ' + author.surname + ' ' + author.birth_year + '-')
 elif sys.argv[1] == 'booksinrange':
     if len(sys.argv) > 4:
-        print('Syntax Error: too many arguments for booksinrange')
+        print('\nSyntax Error: too many arguments for booksinrange\n')
         print('NAME\n\tbooksinrange - Search range of years\nSYNOPSIS\n\tbooksinrange <A> <B>\nDESCRIPTION\n\tbooksinrange prints a list of books published between years <A> and <B>, inclusive')
     else:
         if len(sys.argv) == 2:
             listOfBooks = BooksDataSource('books1.csv').books_between_years('')
         elif len(sys.argv) == 3:
-            if sys.argv[2].isnumeric != True:
-                print('Syntax Error: start year must be a number')
+            if sys.argv[2].isnumeric() != True:
+                print('\nSyntax Error: start year must be a number\n')
                 print('NAME\n\tbooksinrange - Search range of years\nSYNOPSIS\n\tbooksinrange <A> <B>\nDESCRIPTION\n\tbooksinrange prints a list of books published between years <A> and <B>, inclusive')
             else:
                 listOfBooks = BooksDataSource('books1.csv').books_between_years(sys.argv[2])
                 for book in listOfBooks:
                     print(book.title + ' ' + book.publication_year)
         elif len(sys.argv) == 4:
-            if sys.argv[2].isnumeric != True or sys.argv[3].isnumeric != True:
-                print('Syntax Error: start year and end year must be numbers')
+            if sys.argv[2].isnumeric() != True or sys.argv[3].isnumeric() != True:
+                print('\nSyntax Error: start year and end year must be numbers\n')
                 print('NAME\n\tbooksinrange - Search range of years\nSYNOPSIS\n\tbooksinrange <A> <B>\nDESCRIPTION\n\tbooksinrange prints a list of books published between years <A> and <B>, inclusive')
             else:
                 listOfBooks = BooksDataSource('books1.csv').books_between_years(sys.argv[2], sys.argv[3])
                 for book in listOfBooks:
                     print(book.title + ' ' + book.publication_year)
 elif sys.argv[1] == 'use':
-    print('NAME\n\tbooks - Search for books \nSYNOPSIS\n\tbooks <S> <C>\nDESCRIPTION\n\tbooks takes a search string <S> and prints a list of books whose titles contain the string and sorts by criteria <C>.\n\t <C> can be <title> or <year>\n')
-    print('NAME\n\tauthors - Search for authors\nSYNOPSIS\n\tauthors <S>\nDESCRIPTION\n\tauthors takes a search string S and prints a list of authors whose names contain the string sorted by last name then first name\n')
-    print('NAME\n\tbooksinrange - Search range of years\nSYNOPSIS\n\tbooksinrange <A> <B>\nDESCRIPTION\n\tbooksinrange prints a list of books published between years <A> and <B>, inclusive\n')
-    print('NAME\n\tuse - Request usage statement\nSYNOPSIS\n\tuse <flag>\nDESCRIPTION\n\tGives user description of <flag> command-line argument')
+    printUsageStatement()
 else:
-    pass
-
+    print("\nInvalid Input: '" + sys.argv[1] + "' is not a function\n")
+    printUsageStatement()
